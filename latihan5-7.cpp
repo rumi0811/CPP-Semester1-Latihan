bebb15787cd2562e95db88f6c44a26d103dc4e48
Sekar Puspita Arum <94540556+rumi0811@users.noreply.github.com>
2021-12-31 16:31:21 +0700

    Tampilan for bilangan

---
+++ latihan5-7.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+int a, b;
+for(a = 1; a <= 5; a++)
+{
+printf("\n");
+for(b = a; b <= 5; b++)
+cout<<a<<" ";
+}
+getch();
+}
+
