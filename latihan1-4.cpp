90a440b3dbe8aca2badae9236718dd92335f5437
Sekar Puspita Arum <94540556+rumi0811@users.noreply.github.com>
2021-12-31 16:22:10 +0700

    Tampilan sederhana

---
+++ latihan1-4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Selamat Datang di Bina Insani" << endl;
+    cout<<"Algoritma dan Pemrograman 2" << endl;
+    cout<<"Menggunakan Code::Blocks" << endl;
+    return 0;
+}
