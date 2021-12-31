90f496aaec9ee1dc39a4a79f6540989ba58c51b1
Sekar Puspita Arum <94540556+rumi0811@users.noreply.github.com>
2021-12-31 16:38:28 +0700

    Tampilan for 2 if else

---
+++ latihan5-16.cpp
@@ -0,0 +1,21 @@
+#include <conio.h>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+int a, b, I, J, N;
+N=1;
+for (I=1; I<=5; I++)
+for (J=1; J<=5; I++)
+{
+if (a<20)
+cout<<a<<"+";
+else
+cout<<a;
+b=b+a;
+}
+cout<<"="<<b;
+getch();
+}
