7c765ca0947d2dc726c3135c8221f29a7768108a
Sekar Puspita Arum <94540556+rumi0811@users.noreply.github.com>
2021-12-31 16:19:04 +0700

    Pembuatan Biodata simpel

---
+++ latihan1-1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+main ()
+{
+    printf("*********BIODATA MAHASISWA**********\n");
+    printf("NIM : 2021310029\n");
+    printf("Nama : Sekar Puspita Arum\n");
+    printf("Alamat : Komplek Kostrad 04/07 Kebayoran Lama Selatan\n");
+    printf("Email : sekarpuspitaarum08@gmail.com\n");
+    printf("Mata Kuliah :\n");
+    printf("1. Sistem Managemen Basis Data\n");
+    printf("2. Logika Informatika\n");
+    printf("3. Statiska dan Probabilitas\n");
+    printf("4. Bahasa Inggris\n");
+    printf("5. Pengantar ICT\n");
+    printf("6. Pengantar Jaringan\n");
+    printf("7. Logika Informatika\n");
+}
