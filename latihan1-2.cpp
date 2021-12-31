73d91c3968892b8bf246288a4a775d59ed1d99e7
Sekar Puspita Arum <94540556+rumi0811@users.noreply.github.com>
2021-12-31 16:20:02 +0700

    Program hitung luas persegi panjang

---
+++ latihan1-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int panjang;
+    int lebar;
+    int luas;
+
+    cout << "Masukkan Panjang       :";
+    cin >> panjang;
+    cout << "Masukkan Lebar         :";
+    cin >> lebar;
+    luas = panjang * lebar;
+    cout << "Luasnya Adalah         :" << luas << endl;
+
+return 0;
+}
+
+
