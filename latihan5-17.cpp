e342ef94bf7538fbeb5fbc0cdd1342f0b1e8785e
Sekar Puspita Arum <94540556+rumi0811@users.noreply.github.com>
2021-12-31 16:39:07 +0700

    Tampilan for if

---
+++ latihan5-17.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int i,j;
+    for (i=1; i<=3; i++)
+{
+    for (j=i; j<=5; j++)
+{
+    printf("%3i",j);
+}
+    printf("\n");
+}
+
+}
