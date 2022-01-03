58b3cd1dc2342e5256f3c3d0d9359410895736fe
Sekar Puspita Arum <94540556+rumi0811@users.noreply.github.com>
2022-01-03 20:26:23 +0700

    Tampilan function sederhana

---
+++ latihan10-1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+void garis()
+{
+    cout<<"------------------------------"<<endl;
+}
+void nama()
+{
+    cout<<"Semoga Aku Bisa Menguasai C++"<<endl;
+}
+int main()
+{
+    garis();
+    cout<<"Hai Aku Sedang Belajar C++"<<endl;
+    garis();
+    nama();
+    garis();
+    nama();
+    garis();
+    return 0;
+}
